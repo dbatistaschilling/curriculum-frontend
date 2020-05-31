c04b1dafb95f93111b22347836776d82757c5a1c
davi schilling <dbatistaschilling@gmail.com>
2020-05-31 21:35:25 +0200

    commit

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,6 @@ const axios = require('axios').default;
 const instance = axios.create({
     baseURL: process.env.REACT_APP_SERVER_URL,
     // headers: {'X-Custom-Header': 'foobar'}
-  });
+  }); 
 
 export default instance;
